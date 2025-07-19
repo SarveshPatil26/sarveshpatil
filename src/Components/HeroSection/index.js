1ea7d5b8003646156a597d1b297ddf7bd804ff63
SarveshPatil26 <107451867+SarveshPatil26@users.noreply.github.com>
2025-07-19 19:27:03 +0530

    Add files via upload

---
+++ src/Components/HeroSection/index.js
@@ -0,0 +1,222 @@
+import React, { useRef } from 'react';
+import styled from 'styled-components';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Bio } from '../../data/constants';
+import Typewriter from 'typewriter-effect';
+import * as THREE from 'three';
+
+
+const HeroContainer = styled.div`
+  background: ${({ theme }) => theme.card_light};
+  display: flex;
+  justify-content: center;
+  position: relative;
+  padding: 80px 30px;
+  @media (max-width: 960px) {
+    padding: 66px 16px;
+  }
+  @media (max-width: 640px) {
+    padding: 32px 16px;
+  }
+  z-index: 1;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 95%, 0 100%);
+`;
+
+const HeroInnerContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1100px;
+
+  @media (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const HeroLeftContainer = styled.div`
+  width: 100%;
+  order: 1;
+  @media (max-width: 960px) {
+    order: 2;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  @media (max-width: 640px) {
+    order: 2;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const HeroRightContainer = styled.div`
+  width: 100%;
+  display: flex;
+  order: 2;
+  justify-content: end;
+  gap: 12px;
+  @media (max-width: 960px) {
+    order: 1;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 80px;
+  }
+
+
+  @media (max-width: 640px) {
+    margin-bottom: 30px;
+  }
+`;
+
+
+const Title = styled.div`
+  font-weight: 700;
+  font-size: 50px;
+  color: ${({ theme }) => theme.text_primary};
+  line-height: 68px;
+  @media (max-width: 960px) {
+    text-align: center;
+  }
+
+  @media (max-width: 640px) {
+    font-size: 40px;
+    line-height: 48px;
+    margin-bottom: 8px;
+  }
+`;
+
+const TextLoop = styled.div`
+  font-weight: 600;
+  font-size: 32px;
+  display: flex;
+  gap: 12px;
+  color: ${({ theme }) => theme.text_primary};
+  line-height: 68px;
+  @media (max-width: 960px) {
+    text-align: center;
+  }
+  @media (max-width: 640px) {
+    font-size: 22px;
+    line-height: 48px;
+    margin-bottom: 16px;
+  }
+`;
+
+const Span = styled.span`
+  color: ${({ theme }) => theme.primary};
+  cursor: pointer;
+`;
+
+const SubTitle = styled.div`
+  font-size: 20px;
+  line-height: 32px;
+  margin-bottom: 42px;
+  color: ${({ theme }) => theme.text_primary + 95};
+
+  @media (max-width: 960px) {
+    text-align: center;
+  }
+
+  @media (max-width: 640px) {
+    font-size: 16px;
+    line-height: 32px;
+  }
+`;
+
+const ResumeButton = styled.a`
+  -webkit-appearance: button;
+  -moz-appearance: button;
+  appearance: button;
+  text-decoration: none;
+  width: 95%;
+  max-width: 300px;
+  text-align: center;
+  padding: 16px 0;
+  color:${({ theme }) => theme.white};
+  border-radius: 20px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 600;
+  transition: all 0.2s ease-in-out !important;
+  background: hsla(271, 100%, 50%, 1);
+  background: linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  background: -moz-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  background: -webkit-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+  box-shadow:  20px 20px 60px #1F2634,
+               -20px -20px 60px #1F2634;
+  &:hover {
+    transform: scale(1.05);
+    transition: all 0.4s ease-in-out;
+    box-shadow:  20px 20px 60px #1F2634,
+    filter: brightness(1);
+  }    
+    
+  @media (max-width: 640px) {
+    padding: 12px 0;
+    font-size: 18px;
+  } 
+`;
+
+function RotatingIcosahedron() {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef} position={[0, 0, 0]}>
+      <icosahedronGeometry args={[2, 0]} />
+      <meshBasicMaterial color="#FFFFFF" wireframe /> {/* Updated color */}
+    </mesh>
+  );
+}
+
+function Hero() {
+  return (
+    <div id="about">
+      <HeroContainer>
+        <HeroInnerContainer>
+          <HeroLeftContainer id="Left">
+            <Title>Hi, I am <br /> {Bio.name}</Title>
+            <TextLoop>
+              a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target='display'>Check Resume</ResumeButton>
+          </HeroLeftContainer>
+
+          <HeroRightContainer id="Right">
+            <Canvas style={{ width: '500px', height: '500px' }}>
+              <ambientLight intensity={0.5} />
+              <pointLight position={[10, 10, 10]} />
+              <RotatingIcosahedron />
+              <OrbitControls />
+            </Canvas>
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+}
+
+export default Hero;
